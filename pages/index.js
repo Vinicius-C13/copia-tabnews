5f8b48073e4b8dabf3c39f225c892250087387e0
Vinícius da Costa <96350207+Vinicius-C13@users.noreply.github.com>
2025-10-21 23:12:54 +0000

    adicionar pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+//Exemplo de componente básico do React
+function Home() {
+    return <h1>Olá mundo!</h1>
+}
+
+//O export precisa ter o default para definir que apenas esse componente será exportado
+export default Home
