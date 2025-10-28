938f21faf23589dee2ea373c61f0a1ca20222df5
Vinícius da Costa <96350207+Vinicius-C13@users.noreply.github.com>
2025-10-28 22:50:26 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # copia-tabnews
+
 Projeto focado em implementar o https://www.tabnews.com.br/ para o https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 //Exemplo de componente básico do React
 function Home() {
-    return <h1>Hello, world!👋</h1>
+  return <h1>Hello, world!👋</h1>;
 }
 
 //O export precisa ter o default para definir que apenas esse componente será exportado
-export default Home
+export default Home;
